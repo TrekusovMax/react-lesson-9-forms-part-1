7025f0b7cfb0851f81d1308d550e918fa02362a4
Maxim Trekusov <trekusovmax@gmail.com>
2022-01-16 22:31:37 +0300

    added Login page and search people

---
+++ src/app/components/textField.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const toggleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
+    const getInputClasses = () => {
+        return "form-control " + (error ? "is-invalid" : "");
+    };
+    return (
+        <div className="mb-4">
+            <div className="input-group has-validation">
+                <label style={{ marginRight: "10px" }} htmlFor={name}>
+                    {label}
+                </label>
+                <input
+                    onChange={onChange}
+                    id={name}
+                    name={name}
+                    type={showPassword ? "text" : type}
+                    value={value}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        id="inputGroupFileAddon04"
+                        onClick={toggleShowPassword}
+                    >
+                        <i
+                            className={
+                                "bi bi-eye" + (showPassword ? "-slash" : "")
+                            }
+                        ></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
+        </div>
+    );
+};
+
+export default TextField;
+
+TextField.defaultProps = {
+    type: "text"
+};
+
+TextField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    error: PropTypes.string
+};
